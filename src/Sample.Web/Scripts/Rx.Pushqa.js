94249ba9bc04860ca24be21e5a41fa6f6f803a1b
petegoo <pete@petegoo.com>
2012-04-02 23:16:35 +1200

    Added initial cut of RxJs support #3

---
+++ src/Sample.Web/Scripts/Rx.Pushqa.js
@@ -0,0 +1,29 @@
+﻿﻿/// <reference path="rx.min.js" />
+/// <reference path="jquery-1.6.2.js" />
+(function() {
+    /// <param name="$" type="jQuery" />
+    "use strict";
+
+    if (typeof($.signalR) !== "function") {
+        throw "SignalR: SignalR is not loaded. Please ensure jquery.signalR.js is referenced before Rx.Pushqa.";
+    }
+    
+    if (typeof(Rx) !== "object") {
+        throw "Rx: RxJs is not loaded. Please ensure rx.min.js is referenced before Rx.Pushqa.";
+    }
+
+    $.signalR.prototype.subject = new Rx.Subject();
+
+    $.signalR.prototype.asObservable = function () {
+        this.received(function (data) {
+            if (data == 'Pushqa:StreamComplete') {
+                this.subject.onCompleted();
+                this.stop();
+            }
+            else {
+                this.subject.onNext(data);
+            }
+        });
+        return this.subject.asObservable();
+    };
+})();
